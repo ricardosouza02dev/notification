02eb857c6157b7b2cdb2e3b9cbb3e14616e408e5
Ricardo Souza <souzanunez@hotmail.com>
2021-07-24 09:53:43 -0300

    Library OK

--- _app/Email.php
+++ _app/Email.php
@@ -9,21 +9,21 @@
 
         private $mail = \stdClass::class;
 
-        public function __construct()
+        public function __construct($smtpDebug, $host, $user, $pass, $smtpSecure, $port, $setFromEmail, $setFromName)
         {
             $this->mail = new PHPMailer(true);
-            $this->mail->SMTPDebug = 2;                      //Enable verbose debug output
-            $this->mail->isSMTP();                                            //Send using SMTP
-            $this->mail->Host       = 'smtp-relay.sendinblue.com';                     //Set the SMTP server to send through
-            $this->mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $this->mail->Username   = 'ricardosouza020387@gmail.com';                     //SMTP username
-            $this->mail->Password   = 'zWYnas3H0VxSM29f';                               //SMTP password
-            $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
-            $this->mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            $this->mail->SMTPDebug = $smtpDebug;//Enable verbose debug output
+            $this->mail->isSMTP();//Send using SMTP
+            $this->mail->Host       = $host;//'smtp-relay.sendinblue.com';//Set the SMTP server to send through
+            $this->mail->SMTPAuth   = true;//Enable SMTP authentication
+            $this->mail->Username   = $user;//'ricardosouza020387@gmail.com';//SMTP username
+            $this->mail->Password   = $pass;//'zWYnas3H0VxSM29f';//SMTP password
+            $this->mail->SMTPSecure = $smtpSecure; //PHPMailer::ENCRYPTION_STARTTLS;//Enable implicit TLS encryption
+            $this->mail->Port       = $port; //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
             $this->mail->CharSet = "utf-8";
             $this->mail->setLanguage('br');
             $this->mail->isHTML(true);
-            $this->mail->setFrom('ricardosouza020387@gmail.com', 'Equipe Ricardo Souza');
+            $this->mail->setFrom($setFromEmail, $setFromName);
         }
 
         public function sendMail($subject, $body, $replyEmail, $replyName, $addressEmail, $addressName)
--- test/index.php
+++ test/index.php
@@ -1,10 +1,20 @@
 <?php
 
-    require __DIR__ . '/lib_ext/autoload.php';
+    require __DIR__ . '/../lib_ext/autoload.php';
 
     use Notification\Email;
 
-    $novoEmail = new Email;
+    $novoEmail = new Email(
+        2,
+        "smtp-relay.sendinblue.com",
+        "ricardosouza020387@gmail.com",
+        "zWYnas3H0VxSM29f",
+        "tls",
+        "587",
+        "ricardosouza020387@gmail.com",
+        "Ricardo Souza"
+    );
+
     $novoEmail->sendMail(
         "Assunto de Teste",
         "<p>Esse é um email de <b>Teste</b></p>",
